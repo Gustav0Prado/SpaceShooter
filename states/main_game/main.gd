48d4a03b4484dc5a9403947a579169d064c69d1f
gustavops <guprado.silva@gmail.com>
2025-09-22 11:36:22 -0300

    [fix] ui_quit doesnt exists

--- states/main_game/main.gd
+++ states/main_game/main.gd
@@ -5,5 +5,5 @@ func _on_upper_limit_body_entered(body: Node2D) -> void:
 	print(body.name)
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("ui_quit"):
+	if Input.is_action_just_pressed("ui_cancel"):
 		queue_free()
